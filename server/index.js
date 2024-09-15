12d670fdb42d9945d49dfd720751263773104d05
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2024-09-15 14:08:19 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,8 +9,25 @@ app.use(cors())
 
 mongoose.connect("mongodb://localhost:27017/hostelity");
 
+//formatting the date
+const formatDate = (dateString) => {
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+}
+
 app.post('/login', (req, res) => {
+    //formatting the date before saving
+    const formattedDate = formatDate(req.body.date);
+
+    // replace the original date with the formatted date
+    req.body.date = formattedDate;
     StudentModel.create(req.body)
+    .then(students => res.json(students))
+    .catch(err => res.json(err))
+})
+
+app.get('/students', (req, res) => {
+    StudentModel.find()
     .then(students => res.json(students))
     .catch(err => res.json(err))
 })
--- server/models/Student.js
+++ server/models/Student.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
     name: String,
     email: String,
-    pass: String
+    phone: String,
+    college: String,
+    admission: String,
+    department: String,
+    semester: String,
+    duration: String,    
+    pass: String,
+    confirm: String,
+    mess: String,
+    date: String
 });
 
 const StudentModel = mongoose.model("Student", studentSchema);
