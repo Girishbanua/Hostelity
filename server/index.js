4191ea4952e82adc8dcb6156d410f1cba6487db5
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2024-10-02 11:57:45 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,51 +1,14 @@
 const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const StudentModel = require("./models/Student")
+const router = require("./router/router")
+const connectDB = require("./utilities/DB")
+const dotenv = require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use("/api", router)
 
-mongoose.connect("mongodb://localhost:27017/hostelity");
+const PORT = process.env.PORT || 5000
 
-//formatting the date
-const formatDate = (dateString) => {
-    const [year, month, day] = dateString.split('-');
-    return `${day}/${month}/${year}`;
-}
-
-app.post('/login', (req, res) => {
-    //formatting the date before saving
-    const formattedDate = formatDate(req.body.date);
-    
-    // replace the original date with the formatted date
-    req.body.date = formattedDate;
-    //check if the student already exists
-    StudentModel.findOne({email: req.body.email})
-    .then(student => {
-        if(student) {
-            res.json({message: "Already Registered"})            
-        } else {
-            StudentModel.create(req.body)
-            .then(students => res.json(students))
-            .catch(err => res.json(err))
-        }
-    })
-})    
-app.get('/students', (req, res) => {
-    StudentModel.find()
-    .then(students => res.json(students))
-    .catch(err => res.json(err))
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
-
-// API route to get count on number of students registered
-app.get('/count', async(req, res) => {
-    try {
-        const count = await StudentModel.countDocuments();
-        res.json({count});
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-})
-app.listen(4000, () => console.log("Server started at 4000"))
