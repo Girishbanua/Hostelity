3cf753f814859ce414f3bb0350ba362f5b3fd7bb
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2024-10-07 23:26:55 +0530

    Add files via upload

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>    
-    <App />    
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  </React.StrictMode>
+);
