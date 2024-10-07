9416f336f89360e3cf210a2ca70dda5037d0763b
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2024-10-07 23:32:52 +0530

    Add files via upload

--- server/middlewares/stdntSignupValidate.js
+++ server/middlewares/stdntSignupValidate.js
@@ -1,16 +1,26 @@
+const { z } = require("zod");
+
 const stdntSignupValidate = (schema) => async (req, res, next) => {
     try {
         const parseBody = await schema.parseAsync(req.body);
         req.body = parseBody;
         next();
-    } catch (err) {        
+    } catch (err) {   
+        // if(err instanceof z.ZodError){
+        //     // const errorMsg = err.issues.map((issue) => {
+        //     //     return issue.message
+        //     // })
+        //     err.issues.forEach((errorMsg) => {
+        //         return errorMsg
+        //     }); 
+        //     res.status(400).json({message: errorMsg})
+        // }     
         console.log(err)
         const status = 422
         const message = "Fill the input properly"  
         const extraInfo = err.errors[0].message;
-        const error = {status, message, extraInfo}
-        
-        // res.status(400).json({ error: errorMsg });
+        const error = {status, message, extraInfo}        
+        res.status(400).json({ message: extraInfo });
         next(error)
     }    
 }
